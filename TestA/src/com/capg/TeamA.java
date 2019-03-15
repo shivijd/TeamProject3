0ac43bae39906ffb71c7513eb33b4a86417143c3
SHJADAUN <SHJADAUN@DIN51003479.corp.capgemini.com>
2019-03-15 14:15:26 +0530

    commit

---
+++ TestA/src/com/capg/TeamA.java
@@ -0,0 +1,11 @@
+package com.capg;
+
+public class TeamA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome from team A");
+
+	}
+
+}
